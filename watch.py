a035addd92908e4a7fd00c6b676851d497dd292a
klausondrag <18405064+klausondrag@users.noreply.github.com>
2018-10-05 01:46:06 +0200

    implemented post request

--- watch.py
+++ watch.py
@@ -3,6 +3,8 @@ import logging
 import socket
 import time
 
+import requests
+
 logging.basicConfig(format='%(asctime)s - %(levelname)-8s - %(name)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
@@ -18,12 +20,15 @@ def main() -> None:
 
     logger.info("Port is open")
     base_path = Path('data')
-    input_path = base_path / 'input'
+    input_path = base_path / 'output'
 
     input_file_paths = input_path.glob('*.json')
     input_file_paths = sorted(input_file_paths, reverse=True)
     for input_file_path in input_file_paths:
         logging.info(f'Reading {input_file_path.name}...')
+        response = requests.post(f'http://{connection_info[0]}:{connection_info[1]}/rest/_bulk',
+                                 data=open(str(input_file_path), 'rb'))
+        logger.info(f'{response.status_code}: {response.content}')
 
 
 if __name__ == '__main__':
