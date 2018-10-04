d950747f89a66e8613ddf24a65698c5f4b2885c8
klausondrag <18405064+klausondrag@users.noreply.github.com>
2018-10-04 19:51:55 +0200

    implemented conversion of the csv files

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ logging.basicConfig(format='%(asctime)s - %(levelname)-8s - %(name)s    - %(mess
 logger = logging.getLogger(__name__)
 
 
-def main(source_url: str = 'https://github.com/paperscape/paperscape-data.git') -> None:
+def clean_field(s: str) -> str:
+    return s.replace('"', '').replace('\t', ' ')
+
+
+def main(source_url: str = 'https://github.com/paperscape/paperscape-data.git',
+         n_max_splits: int = 7) -> None:
     base_path = Path('data')
     input_path = base_path / 'input'
     output_path = base_path / 'output'
@@ -20,10 +25,41 @@ def main(source_url: str = 'https://github.com/paperscape/paperscape-data.git') 
         logger.info('Finished cloning repo')
     output_path.mkdir(exist_ok=True, parents=True)
 
-    input_files = input_path.glob('*.csv')
-    input_files = sorted(input_files, reverse=True)
-    for input_file in input_files:
-        logging.info(f'Converting {input_file.name}...')
+    input_file_paths = input_path.glob('*.csv')
+    input_file_paths = sorted(input_file_paths, reverse=True)
+    for input_file_path in input_file_paths:
+        logging.info(f'Converting {input_file_path.name}...')
+        year = input_file_path.name[5:9]
+
+        output_file_path = output_path / f'{year}.json'
+        with open(str(output_file_path), 'w') as output_file:
+            output_file.write('[')
+
+            with open(str(input_file_path), 'r') as input_file:
+                for line in input_file:
+                    line_clean = line.strip()
+                    if not line_clean.startswith('#'):
+                        fields = line_clean.split(';', n_max_splits)
+                        arxiv_id = clean_field(fields[0])
+                        authors = clean_field(fields[5])
+                        authors = authors.replace(',', ', ')
+                        title = clean_field(fields[-1])
+
+                        document = f"""
+  {{
+    "type": "PUT",
+    "document": {{
+      "id": "{arxiv_id}",
+      "fields": {{
+        "year": "{year}",
+        "authors": "{authors}",
+        "title": "{title}",
+      }}
+    }}
+  }},"""
+                        output_file.write(document)
+                        
+            output_file.write('\n]')
 
 
 if __name__ == '__main__':
