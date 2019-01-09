cc00fe07370013cbf2b13b072c9d6b492ee1908d
Klaus Ondrag <18405064+klausondrag@users.noreply.github.com>
2019-01-09 14:51:58 +0100

    added logging of duration in watch.py

--- watch.py
+++ watch.py
@@ -14,7 +14,7 @@ def main() -> None:
     connection_info = ('search', 10002)
     result = sock.connect_ex(connection_info)
     while result != 0:
-        logger.info("Port is not open")
+        logger.info('Port is not open')
         time.sleep(1)
         result = sock.connect_ex(connection_info)
 
@@ -22,16 +22,22 @@ def main() -> None:
     for _ in range(5):
         logger.info('.')
         time.sleep(1)
+
     base_path = Path('data')
-    input_path = base_path / 'output'
+    input_path = base_path / 'input'
 
     input_file_paths = input_path.glob('*.json')
-    input_file_paths = sorted(input_file_paths, reverse=True)
+    input_file_paths = sorted(input_file_paths, reverse=False)
+
     for input_file_path in input_file_paths:
         logging.info(f'Reading {input_file_path.name}...')
+        start_time = time.time()
         response = requests.post(f'http://{connection_info[0]}:{connection_info[1]}/rest/_bulk',
                                  data=open(str(input_file_path), 'rb'))
         logger.info(f'{response.status_code}: {response.content}')
+        end_time = time.time()
+        duration = end_time - start_time
+        logging.info(f'Time passed: {duration:.02f}')
 
 
 if __name__ == '__main__':
