d153243eefce0b81eda2496b953f903dbcd75a33
Klaus Ondrag <18405064+klausondrag@users.noreply.github.com>
2019-01-10 04:11:21 +0100

    merged watch files

--- Dockerfile
+++ Dockerfile
@@ -3,30 +3,27 @@ RUN apk add --no-cache git libpq
 RUN apk add --virtual build-deps gcc python3-dev musl-dev postgresql-dev
 RUN pip install pipenv
 WORKDIR /usr/src/app
-COPY Pipfile .
-COPY Pipfile.lock .
+COPY ./Pipfile .
+COPY ./Pipfile.lock .
 RUN pipenv install --system --deploy --ignore-pipfile
 RUN apk del build-deps
 
 FROM base as converter
-COPY data ./data
-COPY convert.py .
+COPY ./data ./data
+COPY ./src/convert.py .
 RUN python ./convert.py
 
 FROM base as py
-COPY watch_helper.py .
+COPY ./src/watch.py .
 
 FROM py as blast
 COPY --from=converter /usr/src/app/data/output_for/blast ./data/input
-COPY watch_blast.py .
-CMD ["python", "./watch_blast.py"]
+CMD ["python", "./watch.py", "init-blast"]
 
 FROM py as redis
 COPY --from=converter /usr/src/app/data/output_for/redis ./data/input
-COPY watch_redis.py .
-CMD ["python", "./watch_redis.py"]
+CMD ["python", "./watch.py", "init-redis"]
 
 FROM py as postgres
 COPY --from=converter /usr/src/app/data/output_for/postgres ./data/input
-COPY watch_postgres.py .
-CMD ["python", "./watch_postgres.py"]
+CMD ["python", "./watch.py", "init-postgres"]
---
+++ src/watch.py
@@ -0,0 +1,143 @@
+import csv
+import logging
+import socket
+import time
+from pathlib import Path
+
+import click
+import psycopg2
+import redis
+import requests
+
+logging.basicConfig(format='%(asctime)s - %(levelname)-8s - %(name)s - %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--host', '-h', type=str, default='blast', help='host to connect to')
+@click.option('--port', '-p', type=int, default=10002, help='port to connect to')
+@click.option('--file-glob', '-fg', type=str, default='*.json', help='files to search for')
+def init_blast(host: str, port: int, file_glob: str) -> None:
+    _wait_until_open(host, port)
+    base_path = Path('data')
+    input_path = base_path / 'input'
+    input_file_paths = input_path.glob(file_glob)
+    input_file_paths = sorted(input_file_paths, reverse=False)
+
+    for input_file_path in input_file_paths:
+        start_time = time.time()
+
+        logging.info(f'Reading {input_file_path.name}...')
+        response = requests.post(f'http://{host}:{port}/rest/_bulk', data=open(str(input_file_path), 'rb'))
+        logger.info(f'{response.status_code}: {response.content}')
+
+        end_time = time.time()
+        duration = end_time - start_time
+        logging.info(f'Time passed: {duration:.02f}')
+
+
+@cli.command()
+@click.option('--host', '-h', type=str, default='redis', help='host to connect to')
+@click.option('--port', '-p', type=int, default=6379, help='port to connect to')
+@click.option('--file-glob', '-fg', type=str, default='*.json', help='files to search for')
+def init_redis(host: str, port: int, file_glob: str) -> None:
+    _wait_until_open(host, port)
+    base_path = Path('data')
+    input_path = base_path / 'input'
+    input_file_paths = input_path.glob(file_glob)
+    input_file_paths = sorted(input_file_paths, reverse=False)
+
+    connection = redis.Redis(host=host, port=port, db=0)
+
+    for input_file_path in input_file_paths:
+        start_time = time.time()
+
+        logging.info(f'Reading {input_file_path.name}...')
+        with open(str(input_file_path), newline='') as input_file:
+            file_reader = csv.reader(input_file)
+            for arxiv_id, year, authors, title in file_reader:
+                data = {'year': year, 'authors': authors, 'title': title}
+                connection.hmset(arxiv_id, data)
+
+        end_time = time.time()
+        duration = end_time - start_time
+        logging.info(f'Time passed: {duration:.02f}')
+
+
+@cli.command()
+@click.option('--host', '-h', type=str, default='postgres', help='host to connect to')
+@click.option('--port', '-p', type=int, default=5432, help='port to connect to')
+@click.option('--file-glob', '-fg', type=str, default='*.sql', help='files to search for')
+def init_postgres(host: str, port: int, file_glob: str) -> None:
+    _wait_until_open(host, port)
+    base_path = Path('data')
+    input_path = base_path / 'input'
+    input_file_paths = input_path.glob(file_glob)
+    input_file_paths = sorted(input_file_paths, reverse=False)
+
+    connection = psycopg2.connect(f"host='{host}' port={port} dbname=postgres user=postgres password=mysecretpassword")
+
+    important_files = ['create_tables.sql', 'insert_into_papers.sql']
+    for input_file_name in important_files:
+        input_file_path = input_path / input_file_name
+        logging.info(f'Reading {input_file_path.name}...')
+        cursor = connection.cursor()
+        cursor.execute(open(str(input_file_path), 'r').read())
+        connection.commit()
+        cursor.close()
+
+    for input_file_path in input_file_paths:
+        start_time = time.time()
+
+        if input_file_path.name in important_files:
+            continue
+
+        logging.info(f'Reading {input_file_path.name}...')
+        cursor = connection.cursor()
+        cursor.execute(open(str(input_file_path), 'r').read())
+        connection.commit()
+        cursor.close()
+
+        end_time = time.time()
+        duration = end_time - start_time
+        logging.info(f'Time passed: {duration:.02f}')
+
+    connection.close()
+
+
+def _wait_until_open(host: str, port: int) -> None:
+    start_time = time.time()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection_info = (host, port)
+    try:
+        result = sock.connect_ex(connection_info)
+    except socket.gaierror as error:
+        if error.errno == -2:
+            logger.error(f'Name or service "{host}" not known. Either the docker-compose file is wrong'
+                         + ' or this file is run outside of docker.')
+            exit(-2)
+        else:
+            raise error
+
+    while result != 0:
+        logger.info('Port is not open')
+        time.sleep(1)
+        result = sock.connect_ex(connection_info)
+
+    logger.info('Port is open')
+    for _ in range(5):
+        logger.info('.')
+        time.sleep(1)
+
+    end_time = time.time()
+    duration = end_time - start_time
+    logging.info(f'Time passed: {duration:.02f}')
+
+
+if __name__ == '__main__':
+    cli()
