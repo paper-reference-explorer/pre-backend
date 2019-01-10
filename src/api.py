e70b4bf6a177057cfcbc5a95bde50b64dd1d78da
Klaus Ondrag <18405064+klausondrag@users.noreply.github.com>
2019-01-10 15:56:54 +0100

    implemented references endpoint

--- src/api.py
+++ src/api.py
@@ -1,7 +1,8 @@
 import copy
 import json
-from typing import Dict
+from typing import Dict, Optional
 
+import psycopg2
 import redis
 import requests
 from flask import Flask, jsonify, abort
@@ -9,8 +10,7 @@ from flask import Flask, jsonify, abort
 app = Flask(__name__)
 redis_connection = redis.StrictRedis('redis', 6379, 0, charset='utf-8', decode_responses=True)
 
-blast_host = 'blast'
-blast_port = 10002
+blast_url = f'http://blast:10002/rest/_search'
 
 redis_host = 'redis'
 redis_port = 6379
@@ -34,6 +34,10 @@ blast_request = {
     }
 }
 
+postgres_connection_string = "host='postgres' port=5432 dbname=postgres user=postgres password=mysecretpassword"
+postgres_connection = psycopg2.connect(postgres_connection_string)
+postgres_select_sql = "SELECT referencee FROM refs WHERE referencer = %(paper_id)s"
+
 
 @app.route('/')
 def hello_world():
@@ -52,7 +56,7 @@ def autocomplete(query: str):
     payload['search_request']['query']['query'] = query
     payload = json.dumps(payload)
 
-    blast_response = requests.post(f'http://{blast_host}:{blast_port}/rest/_search', data=payload)
+    blast_response = requests.post(blast_url, data=payload)
     if blast_response.status_code != 200:
         abort(blast_response.status_code)
 
@@ -60,6 +64,7 @@ def autocomplete(query: str):
     if blast_response['success']:
         hits = blast_response['search_result']['hits']
         result = [_get_paper(h['id']) for h in hits]
+        result = [r for r in result if r is not None]
     else:
         result = []
 
@@ -68,13 +73,28 @@ def autocomplete(query: str):
 
 @app.route('/api/v1/references/<string:paper_id>')
 def references(paper_id: str):
-    return f'Hello {paper_id}!'
+    cursor = postgres_connection.cursor()
+
+    cursor.execute(postgres_select_sql, dict(paper_id=paper_id))
+    postgres_result = cursor.fetchall()
+    if len(postgres_result) > 0:
+        result = [_get_paper(r[0]) for r in postgres_result]
+        result = [r for r in result if r is not None]
+    else:
+        result = []
+
+    postgres_connection.commit()
+    cursor.close()
+    return jsonify(result)
 
 
-def _get_paper(paper_id: str) -> Dict[str, str]:
+def _get_paper(paper_id: str) -> Optional[Dict[str, str]]:
     p = redis_connection.hgetall(paper_id)
-    p['id'] = paper_id
-    return p
+    if len(p.keys()) > 0:
+        p['id'] = paper_id
+        return p
+    else:
+        return None
 
 
 if __name__ == '__main__':
